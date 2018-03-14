8dd2fc03846bf269a910effea29cf8084baec8b9
Lavell <Lavell@Lavells-MacBook-Pro.local>
2018-03-14 14:38:28 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//Forgot to add the question
+/*It was something like create a function that takes a number and makes it negative
+if the number is greater than 0. If the number is less than 0 than keep it the same.
+*/
+function makeNegative(num){
+  if(num > 0){
+    return num * -1
+  }else{
+    return num
+  };
+};
+makeNegative(42)
